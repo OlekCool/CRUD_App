1f55bb14dc5e892a5e3bbcb0773888c1adecfd18
Oleksandr Borovyk <sasha.borovyk03@gmail.com>
2024-12-08 06:48:18 +0200

    2. CRUD_APP    (added creating person and adding it to list)    (data storage is in PersonDAO class temporarily)

--- src/main/java/ua/borovyk/springcourse/controllers/PeopleController.java
+++ src/main/java/ua/borovyk/springcourse/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ua.borovyk.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ua.borovyk.springcourse.dao.PersonDAO;
+import ua.borovyk.springcourse.models.Person;
 
 /**
  * @author Oleksandr Borovyk
@@ -31,5 +30,16 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson (@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
